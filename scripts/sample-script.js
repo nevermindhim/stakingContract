7881d18b793d097809cdbffcaa59c3b39d83b5c2
verytinydever <tinydever@gmail.com>
2021-10-28 02:50:34 +0300

    simple deploy script

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -5,6 +5,8 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+const totalSupply = Number(process.env.SUPPLY) || 1000000 // 1M by default
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -13,13 +15,22 @@ async function main() {
   // manually to make sure everything is compiled
   await hre.run('compile');
 
-  // We get the contract to deploy
+  const [ deployer ] = await hre.ethers.getSigners();
+  console.log(`Deploying contracts with the account: ${deployer.address}`);
+  console.log(`Account balance: ${hre.ethers.utils.formatEther((await deployer.getBalance()).toString())}`);
+
+  // deploy token itself
   const TKNContract = await hre.ethers.getContractFactory("TKN");
-  const token = await TKNContract.deploy(100000);
+  const token = await TKNContract.deploy(totalSupply);
 
   await token.deployed();
+  console.log(`Token contract deployed on the address ${token.address} with supply ${totalSupply}`);
 
-  console.log("ERC20 token deployed to:", token.address);
+  // deploy staking contract
+  const StakingContract = await hre.ethers.getContractFactory('Staking');
+  const staking = await StakingContract.deploy(token.address);
+  await staking.deployed();
+  console.log(`Staking contract deployed on the address ${staking.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
