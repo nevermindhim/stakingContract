bc788993dd0d6ec60a8c524f11023c7b1f85a6df
verytinydever <tinydever@gmail.com>
2021-10-27 23:32:33 +0300

    added comments

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -29,11 +29,19 @@ contract Staking is Ownable {
     event Unstake(address indexed staker, uint amount);
     event Distribute(uint indexed amount);
 
+    /**
+     * @dev Initializes interface to token we are going to interact with,
+     * also get rid of zero index.
+     */
     constructor(address tokenAddress) {
         stakingToken = IERC20(tokenAddress);
-        stakeHolders.push(); // get rid of zero index
+        stakeHolders.push();
     }
 
+    /**
+     * @dev Returns index of stake holder with address (`_address`)
+     * returns zero index if stake holder not exist
+     */
     function getStakeHolderIndex(address _address) private view returns (uint) {
         for (uint i = 1; i < stakeHolders.length; i += 1){
             if (_address == stakeHolders[i]._address) {
@@ -43,6 +51,11 @@ contract Staking is Ownable {
         return 0;
     }
 
+    /**
+     * @dev Stake specific amount (`amount`) of tokens from caller address to contract address
+     * User can stake if didn't stake before or if his stake equals to zero
+     * If user already stake some tokens he can't stake again
+     */
     function stake(uint amount) public {
         uint index = getStakeHolderIndex(msg.sender);
         bool holderNotExist = index == 0;
@@ -62,11 +75,17 @@ contract Staking is Ownable {
         emit Stake(msg.sender, amount);
     }
     
+    /**
+     * @dev Returns reward for the particular stake holder (`StakeHolder`)
+     * using decimals to bring values back to normal format
+     */
     function calculateReward(StakeHolder memory stakeHolder) internal view returns (uint) {
-        // devide decimals to bring values back to normal format
         return stakeHolder._stake + (stakeHolder._stake * (rewardToBeDistributed - So[stakeHolder._address])) / decimals;
     }
     
+    /**
+     * @dev Returns array of all stake holders with computed reward for all of them
+     */
     function getAllStakers() public view returns (RewardHolder[] memory) {
         RewardHolder[] memory rewardHolders = new RewardHolder[](stakeHolders.length - 1);
         for (uint i = 1; i < stakeHolders.length; i += 1){
@@ -77,14 +96,21 @@ contract Staking is Ownable {
         return rewardHolders;
     }
 
+    /**
+     * @dev Distributes reward (`reward`) proportionally to all stakers
+     * multiply float value by decimals to not to lose fractional part
+     */
     function distribute(uint reward) onlyOwner public {
         require(allActiveStakes != 0, 'You need at least one ');
 
-        rewardToBeDistributed += reward * decimals / allActiveStakes; // multiply by decimals to not to lose decimals
+        rewardToBeDistributed += reward * decimals / allActiveStakes;
         stakingToken.transferFrom(msg.sender, address(this), reward);
         emit Distribute(reward);
     }
 
+    /**
+     * @dev Unstake all tokens the user received during staking period
+     */
     function unstake() public {
         uint index = getStakeHolderIndex(msg.sender);
         StakeHolder storage holder = stakeHolders[index];
