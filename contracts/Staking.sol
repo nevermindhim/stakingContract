6530f7b34d492fde4ae99f0f96b32a566e8fc63a
verytinydever <tinydever@gmail.com>
2021-10-27 18:06:41 +0300

    functionality implemented

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,30 +1,38 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.4;
+pragma abicoder v2;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-// TODO: if user makes 2 stakes with the same address should I sum stakes or revert
 
 contract Staking is Ownable {
     IERC20 public stakingToken;
-    uint allActiveStakes;
-    uint rewardToBeDistributed;
+
+    uint public allActiveStakes;
+    uint private rewardToBeDistributed;
+    uint constant decimals = 1e18;
 
     struct StakeHolder {
         address _address;
-        uint stake;
+        uint _stake;
+    }
+    
+    struct RewardHolder {
+        address _address;
+        uint _reward;
     }
 
     StakeHolder[] internal stakeHolders;
     mapping(address => uint) internal So;
+    // TODO: add events
 
     constructor(address tokenAddress) {
         stakingToken = IERC20(tokenAddress);
-        stakeHolders.push(); // avoid 0 index
+        stakeHolders.push(); // get rid of zero index
     }
 
-    function getStakeHolder(address _address) public view returns(uint) {
-        for (uint i = 0; i < stakeHolders.length; i += 1){
+    function getStakeHolder(address _address) private view returns (uint) {
+        for (uint i = 1; i < stakeHolders.length; i += 1){
             if (_address == stakeHolders[i]._address) {
                 return i;
             }
@@ -32,24 +40,41 @@ contract Staking is Ownable {
         return 0;
     }
 
-    function stake(uint amount) external {
+    function stake(uint amount) public {
+        require(getStakeHolder(msg.sender) == 0, 'Staker already exist');
         stakeHolders.push(StakeHolder(msg.sender, amount));
         So[msg.sender] = rewardToBeDistributed;
         allActiveStakes += amount;
         stakingToken.transferFrom(msg.sender, address(this), amount);
     }
+    
+    function calculateReward(StakeHolder memory stakeHolder) internal view returns (uint) {
+        // devide decimals to bring values back to normal format
+        return stakeHolder._stake + (stakeHolder._stake * (rewardToBeDistributed - So[stakeHolder._address])) / decimals;
+    }
+    
+    function getAllStakers() public view returns (RewardHolder[] memory) {
+        RewardHolder[] memory rewardHolders = new RewardHolder[](stakeHolders.length - 1);
+        for (uint i = 1; i < stakeHolders.length; i += 1){
+            StakeHolder memory stakeHolder = stakeHolders[i];
+            uint reward = calculateReward(stakeHolder);
+            rewardHolders[i - 1] = RewardHolder(stakeHolder._address, reward);
+        }
+        return rewardHolders;
+    }
 
-    function distribute(uint reward) onlyOwner external {
+    function distribute(uint reward) onlyOwner public {
         require(allActiveStakes != 0, 'You need at least one ');
-        rewardToBeDistributed += reward / allActiveStakes;
+        rewardToBeDistributed += reward * decimals / allActiveStakes; // multiply by decimals to not to lose decimals
         stakingToken.transferFrom(msg.sender, address(this), reward);
     }
 
-    function unstake() external {
-        StakeHolder memory holder = stakeHolders[getStakeHolder(msg.sender)];
-        allActiveStakes -= holder.stake;
-        uint amount = holder.stake + holder.stake * (rewardToBeDistributed - So[msg.sender]);
-        holder.stake = 0;
+    // TODO: think about do we need remove stake holder from storage to let them make stakes again
+    function unstake() public {
+        StakeHolder storage holder = stakeHolders[getStakeHolder(msg.sender)];
+        allActiveStakes -= holder._stake;
+        uint amount = calculateReward(holder);
+        holder._stake = 0;
         stakingToken.transfer(msg.sender, amount);
     }
 }
